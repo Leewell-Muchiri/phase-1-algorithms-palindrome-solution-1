ad82e9f2604bc89824d1d58621cc92104e486885
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-30 22:05:22 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
+function reverseString(word) {
+return word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
+ const reversedWord = reverseString(word);
+ return word === reversedWord;
+
   // Write your algorithm here
+}
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
@@ -17,7 +59,6 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
